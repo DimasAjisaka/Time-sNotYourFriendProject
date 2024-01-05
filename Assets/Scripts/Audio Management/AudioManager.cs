a55172810b3ac546bdff8d7f000c90ff5e7df679
DimasAjisaka <81664698+DimasAjisaka@users.noreply.github.com>
2024-01-05 13:43:57 +0700

    Add Audio System
    Menambahkan Audio System ke dalam project

--- Assets/Scripts/Audio Management/AudioManager.cs
+++ Assets/Scripts/Audio Management/AudioManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class AudioManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class AudioManager : MonoBehaviour {
+    public static AudioManager instance;
+
+    public Sound[] bgmAudio, sfxAudio;
+    public AudioSource bgmSource, sfxSource;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PlayBGM(string name) {
+        Sound bgm = Array.Find(bgmAudio, x => x.name == name);
+        if (bgm == null) {
+            Debug.Log("BGM Not Found!");
+        } else {
+            bgmSource.clip = bgm.clip;
+            bgmSource.loop = true;
+            bgmSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name) {
+        Sound sfx = Array.Find(sfxAudio, x => x.name == name);
+        if (sfx == null) {
+            Debug.Log("SFX Not Found!");
+        } else { sfxSource.PlayOneShot(sfx.clip); }
     }
 }
