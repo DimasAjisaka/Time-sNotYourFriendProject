56cb3be4582a78c1ecd9784aff9d35626eab012f
RizkiRamadhan99 <rizkirmdn@gmail.com>
2024-01-05 14:23:28 +0700

    Add Player Move

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isMoving = false;
+    private Vector3 origPos, targerPos;
+    public float timeToMove = 0.2f;
+
+    private void Update()
+    {
+        GetPlayerInput();
+    }
+
+    void GetPlayerInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+        if (Input.GetKeyDown(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+        if (Input.GetKeyDown(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+        if (Input.GetKeyDown(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targerPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targerPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targerPos;
+
+        isMoving = false;
+    }
+}
