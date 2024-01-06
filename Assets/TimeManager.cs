a0707016a26158ddcaf2bb92f56ce29601e61162
Rafalff <rczfa27@gmail.com>
2024-01-06 21:11:12 +0700

    heartbeat logic

--- Assets/TimeManager.cs
+++ Assets/TimeManager.cs
@@ -4,32 +4,52 @@ using System.Threading;
 using UnityEngine;
 using DG.Tweening;
 
-public class TimeManager : MonoBehaviour
-{
+public class TimeManager : MonoBehaviour {
     public static TimeManager instance;
     public float timer;
     public float startShaking = 10f;
     private Camera cam;
 
+    private bool isShaking = false;
 
     private void Awake() {
         instance = this;
     }
+
     private void Start() {
         cam = Camera.main;
     }
+
     private void Update() {
         if (GameManager.instance.state == GameState.START || GameManager.instance.state == GameState.BATTLE) {
             timer -= Time.deltaTime;
-            if (timer < startShaking) {
+
+            if (timer <= startShaking) {
                 CameraShake();
-            }
-            if (timer < 0) {
+                if (isShaking == false) {
+                    StartCoroutine(StartHeartBeat());
+                }
+            } 
+
+            if (timer <= 0) {
                 GameManager.instance.UpdateGameState(GameState.GAMEOVER);
                 AudioManager.instance.PlayPlayerVoice("PlayerDeath");
             }
         }
     }
+
+    IEnumerator StartHeartBeat() {
+        isShaking = true;
+        AudioManager.instance.PlayHeartBeat("Slow");
+        while (timer > 5) {
+            AudioManager.instance.PlayHeartBeat("Slow");
+            yield return new WaitForSeconds(1f);
+        }
+        AudioManager.instance.PlayHeartBeat("Fast");
+        yield return new WaitForSeconds(5f);
+        isShaking = false;
+    }
+
 
     private void CameraShake() {
         // Parameters for the shake
