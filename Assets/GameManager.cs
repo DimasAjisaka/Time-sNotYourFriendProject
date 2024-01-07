46c5de60c6ecc5932488436d16379a115826e51e
Rafalff <rczfa27@gmail.com>
2024-01-07 22:26:04 +0700

    update everitiing

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,15 +10,27 @@ public enum GameState {
 public class GameManager : MonoBehaviour {
     public enum SceneActive { MainMenu, Battle }
 
+    [SerializeField] private GameObject enemyShuffle;
+    [SerializeField] private GameObject enemyBatu;
+    [SerializeField] private GameObject enemyGunting;
+    [SerializeField] private GameObject enemyKertas;
+
+
+
     public static GameManager instance;
     public PlayerController playerController;
     public UIManager uiManager;
     public float diceRollingTime = 1f;
 
+    public float winTime = 10f;
+    public float loseTime = 10f;
+
     public GameState state;
     public static event Action<GameState> OnGameStateChanged;
 
     [SerializeField] private SceneActive scene;
+
+    private GameObject enemyChoiceContainer;
 
     private bool isRollingDice = false;
 
@@ -107,6 +120,7 @@ public class GameManager : MonoBehaviour {
                 StartCoroutine(RollingDice(0));
                 yield break;
             }
+            ZoomOutCamera();
         } 
         //escape
         else {
@@ -142,18 +156,128 @@ public class GameManager : MonoBehaviour {
                 StartCoroutine(RollingDice(1));
                 yield break;
             }
+            ZoomOutCamera();
         }
         isRollingDice = false;
         uiManager.EnableDisableBattleButton(isRollingDice);
     }
 
-
-    public void RockPaperScissorsBattle() {
-        StartCoroutine(RollingDice(0));
+    public void ZoomInCamera() {
+        Camera.main.DOOrthoSize(4.5f, 0.3f);
     }
 
-    public void RockPaperScissorsEscape() {
-        StartCoroutine(RollingDice(1));
+    public void ZoomOutCamera() {
+        Camera.main.DOOrthoSize(5f, 0.3f);
+    }
+
+    public void RockPaperScissorsBattle(int choice) {
+        //0 = batu, 1 = gunting, 2 = kertas
+
+        var enemyChoice = UnityEngine.Random.Range(0, 3);
+        if(choice == 0) {
+            if(enemyChoice == 0) {
+                enemyShuffle.SetActive(false);
+                enemyBatu.SetActive(true);
+
+                playerController.KnockPlayer();
+                TimeManager.instance.CameraShakeDamage();
+
+            } else if(enemyChoice == 1) {
+                enemyShuffle.SetActive(false);
+                enemyGunting.SetActive(true);
+
+                TimeManager.instance.timer += winTime;
+                playerController.KillEnemy();
+                AudioManager.instance.PlayPlayerVoice("PlayerWin");
+
+            } else if (enemyChoice == 2) {
+                enemyShuffle.SetActive(false);
+                enemyKertas.SetActive(true);
+
+                TimeManager.instance.timer -= loseTime;
+                playerController.KnockPlayer();
+                TimeManager.instance.CameraShakeDamage();
+                AudioManager.instance.PlayPlayerVoice("PlayerLose");
+
+            }
+
+            UpdateGameState(GameState.START);
+            uiManager.BattlePanelExit();
+            Invoke("TurnOffGBK", 2f);
+
+        } else if (choice == 1) {
+            if (enemyChoice == 0) {
+                enemyShuffle.SetActive(false);
+                enemyBatu.SetActive(true);
+
+                TimeManager.instance.timer -= loseTime;
+                playerController.KnockPlayer();
+                TimeManager.instance.CameraShakeDamage();
+                AudioManager.instance.PlayPlayerVoice("PlayerLose");
+
+            } else if (enemyChoice == 1) {
+                enemyShuffle.SetActive(false);
+                enemyGunting.SetActive(true);
+
+                playerController.KnockPlayer();
+                TimeManager.instance.CameraShakeDamage();
+
+            } else if (enemyChoice == 2) {
+                enemyShuffle.SetActive(false);
+                enemyKertas.SetActive(true);
+
+                TimeManager.instance.timer += winTime;
+                playerController.KillEnemy();
+                AudioManager.instance.PlayPlayerVoice("PlayerWin");
+
+            }
+
+
+            UpdateGameState(GameState.START);
+            uiManager.BattlePanelExit();
+            Invoke("TurnOffGBK", 2f);
+
+
+        }
+        else if (choice == 2) {
+            if (enemyChoice == 0) {
+                enemyShuffle.SetActive(false);
+                enemyBatu.SetActive(true);
+
+                TimeManager.instance.timer += winTime;
+                playerController.KillEnemy();
+                AudioManager.instance.PlayPlayerVoice("PlayerWin");
+
+            } else if (enemyChoice == 1) {
+                enemyShuffle.SetActive(false);
+                enemyGunting.SetActive(true);
+
+                TimeManager.instance.timer -= loseTime;
+                playerController.KnockPlayer();
+                TimeManager.instance.CameraShakeDamage();
+                AudioManager.instance.PlayPlayerVoice("PlayerLose");
+
+            } else if (enemyChoice == 2) {
+                enemyShuffle.SetActive(false);
+                enemyKertas.SetActive(true);
+
+                playerController.KnockPlayer();
+                TimeManager.instance.CameraShakeDamage();
+
+            }
+            UpdateGameState(GameState.START);
+            uiManager.BattlePanelExit();
+            Invoke("TurnOffGBK", 2f);
+        }
+    }
+
+    private void TurnOffGBK() {
+        enemyShuffle.SetActive(true);
+
+        enemyBatu.SetActive(false);
+        enemyGunting.SetActive(false);
+        enemyKertas.SetActive(false);
+
     }
 }
 
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -14,6 +15,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject startButton;
     [SerializeField] private GameObject restartPanel;
     [SerializeField] private GameObject battlePanel;
+    [SerializeField] private RectTransform battlePanelRect;
 
     [SerializeField] private Button attackButton;
     [SerializeField] private Button escapeButton;
@@ -33,13 +35,26 @@ public class UIManager : MonoBehaviour
 
     private void GameManagerOnGameStateChanged(GameState state) {
         startButton.SetActive(state == GameState.WAIT);
-
         _timerText.gameObject.SetActive(state == GameState.START || state == GameState.BATTLE);
-
         restartPanel.SetActive(state == GameState.GAMEOVER);
 
-        battlePanel.SetActive(state == GameState.BATTLE);
+
+        if(state == GameState.BATTLE) {
+            GameManager.instance.ZoomInCamera();
+            battlePanel.SetActive(true);
+            battlePanelRect.DOAnchorPosY(0f, 1f)
+                           .From(new Vector2(0f, -1100f))
+                           .SetEase(Ease.OutQuint);
+        }
     }
+
+    public void BattlePanelExit() {
+        battlePanelRect.DOAnchorPosY(-1100f, 2f)
+                       .From(new Vector2(0f, 0f))
+                       .SetEase(Ease.InQuint)
+                       .OnComplete(() => battlePanel.SetActive(false));
+    }
+
 
     public void StartButtonClicked() {
         GameManager.instance.UpdateGameState(GameState.START);
