bbc82e0606f3938c707df1325da202e5d5da27e9
Rafalff <rczfa27@gmail.com>
2024-01-07 23:04:28 +0700

    door

--- Assets/Key.cs
+++ Assets/Key.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     private PlayerController playerController;
-
+    private Animator doorAni;
     private void Awake()
     {
         playerController = FindAnyObjectByType<PlayerController>();
+        doorAni = FindAnyObjectByType<Door>().GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
+            doorAni.SetBool("isOpen", true);
             playerController.isKeyPicked = true;
             AudioManager.instance.PlayUnlockLevel("GetKey");
             Debug.Log("Key Picked");
