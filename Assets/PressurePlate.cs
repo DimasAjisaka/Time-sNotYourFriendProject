71ff0b829de89ab9589a689124edd50dd6237c3c
Rafalff <rczfa27@gmail.com>
2024-01-07 19:56:56 +0700

    prefabs

--- Assets/PressurePlate.cs
+++ Assets/PressurePlate.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     [SerializeField] private GameObject gateToOpen;
-
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite pushedSprite;
+    private Sprite originalSprite;
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalSprite = spriteRenderer.sprite;
+    }
     private void OnTriggerEnter2D(Collider2D collision) {
-            gateToOpen.SetActive(false);
+        spriteRenderer.sprite = pushedSprite;
+        gateToOpen.GetComponent<Animator>().SetBool("isActive", true);
         AudioManager.instance.PlayEnviFeedback("Plate");
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
-            gateToOpen.SetActive(true);
+        spriteRenderer.sprite = originalSprite;
+        gateToOpen.GetComponent<Animator>().SetBool("isActive", false);
     }
 }
